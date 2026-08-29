273f2e06d3666702c70fbf37250ed26a473a6e83
Ranjan Mishra <ranjan.mishra.dev@gmail.com>
2026-08-29 09:46:20 +0530

    tried groq, mistral, gemini, openai models

--- Part_1/chatmodels/chat.py
+++ Part_1/chatmodels/chat.py
@@ -2,8 +2,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain.chat_models import init_chat_model
+from langchain_openai import ChatOpenAI
 
-model = init_chat_model('gpt-5.4-mini')
-# res = model.invoke("where is mumbai")
+from langchain_mistralai import ChatMistralAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain_groq import ChatGroq
+
+
+# model = init_chat_model("gpt-5.4-mini");
+# model = ChatOpenAI(model='gpt-5.4-mini')
+
+# model = ChatGoogleGenerativeAI(model='gemini-3.1-flash-image')
+# model = ChatGoogleGenerativeAI(model='gemini-3.1-flash-image')
+
+# model = ChatGoogleGenerativeAI(model = "mistral-medium-latest", max_retries=2)
+# model = ChatGoogleGenerativeAI(model='gemini-3.7-flash', temperature=2.0, max_tokens=100)
+
+model = ChatGroq(model="qwen/qwen3.6-27b")
+
+res = model.invoke('mumbai vs new york')
+print(res.content)
 
 print(model)
+# res = model.invoke("dubai vs mumbai")
